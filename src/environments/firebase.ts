67b50bb0f0729f5b1e5442cb8d6540ef66fddb97
Byron Dover <byrondover+github@gmail.com>
2018-04-07 01:46:29 -0700

    Stop tracking firebase config

--- src/environments/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDGBNA2pNl0WUmxxVcgVQheYUypa1QFvwA',
-  authDomain: 'example-backend-f074f.firebaseapp.com',
-  databaseURL: 'https://example-backend-f074f.firebaseio.com',
-  projectId: 'example-backend-f074f',
-  storageBucket: 'gs://example-backend-f074f.appspot.com/',
-  messagingSenderId: '766966507448'
-};
